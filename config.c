3ee90287b22b20884e0d28f9ed8335dd63822e00
Souheab <souheab@protonmail.com>
2024-07-02 18:11:13 -0400

    Create initial working bar

--- config.c
+++ config.c
@@ -1,3 +1,9 @@
+#include <glib-2.0/glib.h>
+
 static const int numtags = 9;
 static const char *taglabel = "";
 static const char *cssfilepath = "./style.css";
+
+void tagbuttononclick(gpointer data) {
+  g_print("data: %d", *((int*) data));
+}
--- sgbar.c
+++ sgbar.c
@@ -1,44 +1,36 @@
 #include <gtk/gtk.h>
 #include <gio/gio.h>
 #include "config.c"
-#include "gtk/gtkcssprovider.h"
 
 GtkWidget *createtagbutton(int tagnum) {
   GtkWidget *button;
+  GtkStyleContext *stylecontext;
   button = gtk_button_new_with_label(taglabel);
+  stylecontext = gtk_widget_get_style_context(button);
+  gtk_style_context_add_class(stylecontext, "tagbutton");
+  g_signal_connect_swapped(button, "clicked", G_CALLBACK(tagbuttononclick), &tagnum);
   return button;
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window;
-  GtkWidget *button_box;
-  GtkCssProvider *cssprovider = gtk_css_provider_new();
-  GtkStyleContext *stylecontext;
-  GFile *cssfile;
-  // TODO: Figure out size per monitor
+  GtkWidget *box;
+  // TODO: Figure out size based on current monitor
+  // Maybe also create window for each monitor
   int window_width = 1920;
   int window_height = 40;
-
-
-  cssfile = g_file_new_for_path(cssfilepath);
-  gtk_css_provider_load_from_file(cssprovider, cssfile , NULL);
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "sgbar");
   gtk_window_set_default_size(GTK_WINDOW(window), window_width, window_height);
   gtk_window_move(GTK_WINDOW(window) , 10, 10);
 
-  stylecontext = gtk_widget_get_style_context(window);
-  gtk_style_context_add_provider(stylecontext, GTK_STYLE_PROVIDER(cssprovider), GTK_STYLE_PROVIDER_PRIORITY_USER);
-
-  button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
-  gtk_button_box_set_layout(GTK_BUTTON_BOX(button_box), GTK_BUTTONBOX_START);
-  gtk_container_add(GTK_CONTAINER(window), button_box);
+  box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_container_add(GTK_CONTAINER(window), box);
 
   for (int i = 0; i < numtags; i++) {
-    gtk_container_add(GTK_CONTAINER (button_box), createtagbutton(0));
+    gtk_container_add(GTK_CONTAINER (box), createtagbutton(0));
   }
-
 
   gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DOCK);
   gtk_widget_show_all(window);
@@ -46,7 +38,19 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
 int main(int argc, char **argv) {
   GtkApplication *app;
+  GdkScreen *screen;
+  GtkCssProvider *cssprovider = gtk_css_provider_new();
+  GFile *cssfile;
   int status;
+
+  gtk_init(&argc, &argv);
+  screen = gdk_screen_get_default();
+  if (screen == NULL) {
+    g_error("Failed to get screen");
+  }
+  cssfile = g_file_new_for_path(cssfilepath);
+  gtk_css_provider_load_from_file(cssprovider, cssfile , NULL);
+  gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(cssprovider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
   app = gtk_application_new("com.souheab.sgbar", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
