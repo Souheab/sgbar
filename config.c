ce3f28664a796b7f118c220a071d40536b0f116b
Souheab <souheab@protonmail.com>
2024-07-02 23:38:53 -0400

    Add basic workspace switch behavior

--- config.c
+++ config.c
@@ -1,9 +1,10 @@
 #include <glib-2.0/glib.h>
+#include <gtk/gtk.h>
 
-static const int numtags = 9;
+#define NUMTAGS 9
 static const char *taglabel = "";
 static const char *cssfilepath = "./style.css";
 
-void tagbuttononclick(gpointer data) {
-  g_print("data: %d", *((int*) data));
+void tagbuttononclick(GtkWidget *button, gpointer data) {
+  g_print("tag clicked: %d\n", GPOINTER_TO_INT(data));
 }
--- sgbar.c
+++ sgbar.c
@@ -1,6 +1,16 @@
-#include <gtk/gtk.h>
-#include <gio/gio.h>
 #include "config.c"
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <gdk/gdkx.h>
+#include <gio/gio.h>
+#include <gtk/gtk.h>
+
+Display *display;
+Window root_window;
+Atom net_current_desktop_atom;
+GtkWidget *tagbuttons[NUMTAGS];
+
+static GdkFilterReturn x11_event_filter(GdkXEvent *xevent, GdkEvent *event, gpointer data);
 
 GtkWidget *createtagbutton(int tagnum) {
   GtkWidget *button;
@@ -8,8 +18,71 @@ GtkWidget *createtagbutton(int tagnum) {
   button = gtk_button_new_with_label(taglabel);
   stylecontext = gtk_widget_get_style_context(button);
   gtk_style_context_add_class(stylecontext, "tagbutton");
-  g_signal_connect_swapped(button, "clicked", G_CALLBACK(tagbuttononclick), &tagnum);
+  g_signal_connect(button, "clicked", G_CALLBACK(tagbuttononclick),
+                   GINT_TO_POINTER(tagnum));
   return button;
+}
+
+void update_tag_buttons(int currenttag) {
+  g_print("running update_tag_buttons\n");
+  for (int i = 0; i < NUMTAGS; i++) {
+    GtkWidget *button = tagbuttons[i];
+    GtkStyleContext *stylecontext = gtk_widget_get_style_context(button);
+    if (i == currenttag) {
+      gtk_style_context_add_class(stylecontext, "tagbutton-active");
+    } else {
+      gtk_style_context_remove_class(stylecontext, "tagbutton-active");
+    }
+  }
+}
+
+static void setup_x11_event_handling(GtkWidget *widget) {
+  GdkScreen *screen = gtk_widget_get_screen(widget);
+  GdkWindow *root_gdk_window = gdk_screen_get_root_window(screen);
+
+  display = GDK_WINDOW_XDISPLAY(root_gdk_window);
+  root_window = GDK_WINDOW_XID(root_gdk_window);
+
+  if (display == NULL || root_window == 0) {
+    return;
+  }
+
+  net_current_desktop_atom =
+      XInternAtom(display, "_NET_CURRENT_DESKTOP", False);
+
+  gdk_window_add_filter(root_gdk_window, x11_event_filter, NULL);
+
+  XSelectInput(display, root_window, PropertyChangeMask);
+}
+
+static GdkFilterReturn x11_event_filter(GdkXEvent *xevent, GdkEvent *event,
+gpointer data) {
+  XEvent *x11_event = (XEvent *)xevent;
+
+  if (x11_event->type == PropertyNotify) {
+    XPropertyEvent *prop_event = (XPropertyEvent *)x11_event;
+    if (prop_event->atom == net_current_desktop_atom) {
+      Atom actual_type;
+      int actual_format;
+      unsigned long nitems, bytes_after;
+      unsigned char *prop_data = NULL;
+
+      if (XGetWindowProperty(display, root_window, net_current_desktop_atom, 0,
+                             1, False, XA_CARDINAL, &actual_type,
+                             &actual_format, &nitems, &bytes_after,
+                             &prop_data) == Success) {
+        if (prop_data) {
+          long desktop = *(long *)prop_data;
+          g_idle_add((GSourceFunc)update_tag_buttons, GINT_TO_POINTER(desktop));
+          g_print("Current desktop: %lu\n", *((unsigned long *)prop_data));
+          XFree(prop_data);
+        }
+      }
+      return GDK_FILTER_REMOVE;
+    }
+  }
+
+  return GDK_FILTER_CONTINUE;
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
@@ -23,16 +96,28 @@ static void activate(GtkApplication *app, gpointer user_data) {
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "sgbar");
   gtk_window_set_default_size(GTK_WINDOW(window), window_width, window_height);
-  gtk_window_move(GTK_WINDOW(window) , 10, 10);
+  g_signal_connect(window, "realize", G_CALLBACK(setup_x11_event_handling), NULL);
+
 
   box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
   gtk_container_add(GTK_CONTAINER(window), box);
 
-  for (int i = 0; i < numtags; i++) {
-    gtk_container_add(GTK_CONTAINER (box), createtagbutton(0));
+  for (int i = 0; i < NUMTAGS; i++) {
+    GtkWidget *button = createtagbutton(i);
+    tagbuttons[i] = button;
+    gtk_container_add(GTK_CONTAINER(box), button);
   }
 
   gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_DOCK);
+  gtk_widget_show_all(window);
+
+  GdkWindow *gdk_window = gtk_widget_get_window(window);
+
+  if (gdk_window) {
+    gdk_window_set_override_redirect(gdk_window, TRUE);
+    gdk_window_show(gdk_window);
+    gdk_window_move(gdk_window, 0, 30);
+  }
   gtk_widget_show_all(window);
 }
 
@@ -49,8 +134,10 @@ int main(int argc, char **argv) {
     g_error("Failed to get screen");
   }
   cssfile = g_file_new_for_path(cssfilepath);
-  gtk_css_provider_load_from_file(cssprovider, cssfile , NULL);
-  gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(cssprovider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+  gtk_css_provider_load_from_file(cssprovider, cssfile, NULL);
+  gtk_style_context_add_provider_for_screen(screen,
+                                            GTK_STYLE_PROVIDER(cssprovider),
+                                            GTK_STYLE_PROVIDER_PRIORITY_USER);
 
   app = gtk_application_new("com.souheab.sgbar", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
